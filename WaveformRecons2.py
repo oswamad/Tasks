80b80b4eb3c9957fb552b56ea3793279c5162515
oswamad <138606723+oswamad@users.noreply.github.com>
2023-07-04 09:47:14 -0600

    Update WaveformRecons2.py
    Instructions added to task 1

--- WaveformRecons2.py
+++ WaveformRecons2.py
@@ -1,3 +1,9 @@
+#Given a waveform a vector (𝑇̅, 𝑌̅), write a Python function that will perform the following
+#operations:
+#a. Returns a value y for any given t
+#b. Accepts a sampling time 𝛥𝑡, and samples vector 𝑌̅ and returns a sampled 𝑌̅𝑆
+#c. Write a simple test to demonstrate this functionality
+
 import matplotlib.pyplot as plt
 import numpy as np
 from decimal import Decimal
